750b39d872835aed8af5d2e15350af358087b360
rooha <github@panadey.ru>
2025-08-28 17:01:17 +0300

    mongo url

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,7 +11,7 @@ export const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
 
 // Mongo
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+export const MONGODB_URI = process.env.MONGO_URL || 'mongodb://localhost:27017';
 export const DB_NAME = process.env.DB_NAME || 'coworking';
 export const COLLECTION_NAME = process.env.COLLECTION_NAME || 'rooms';
 
